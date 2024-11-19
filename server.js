d74feb4e4089f0e01ebe628d0c4de16563553b72
Gabriel Felip Mano Lasig <88683534+gablmao@users.noreply.github.com>
2024-11-19 13:08:40 +0000

    Update server.js
    updates GET /lessons, adde cors

--- server.js
+++ server.js
@@ -20,37 +20,63 @@ var fs = require("fs");
 var cors = require("cors");
 
 var app = express();
-
-app.use(cors());
-
-
-//logging middleware
-app.use(function(request, response, next) {
-  console.log("New request: ", request.url);
-  next();
-});
+app.set("json spaces", 2);  //pretty print json
+app.use(cors());  //enable CORS to connect to localhost
 
 //where the html is located
 //app.use(express.static('public'));
 
+//logging middleware
+app.use(function (request, response, next) {
+  console.log("New request: ", request.url);
+  next();
+});
+
 
 //don't forget POST PUT DELETE requests !!!!!!
+//to test in terminal type: curl -X POST http://localhost:3000
+
 app.get("/", function (req, res) {
   res.send("if this shows it works, GET");
 });
 
 app.get("/lessons", function (req, res) {
-  res.send("if this shows it works, GET /lessons");
+  //res.send("if this shows it works, GET /lessons");
+
+  let products = [{
+    id: 1001,
+    name: "Math",
+    place: "London",
+    image: "images/placeholder.jpg",
+    slots: 5,
+    price: 10,
+  }, {
+    id: 1002,
+    name: "English Literature",
+    place: "York",
+    image: "images/placeholder.jpg",
+    slots: 3,
+    price: 7,
+  }, {
+    id: 1003,
+    name: "Geography",
+    place: "London",
+    image: "images/placeholder.jpg",
+    slots: 7,
+    price: 8,
+  }];
+
+  res.json(products);
 });
 
 
 
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.status(404).send("Page not found");
 });
 
 
 //listen at port 3000
-http.createServer(app).listen(3000, "0.0.0.0", function() {
+http.createServer(app).listen(3000, "0.0.0.0", function () {
   console.log("Server is running at port 3000");
 });
